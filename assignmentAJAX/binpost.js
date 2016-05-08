d13b2d5d4160f3dc81e8ad6caeef1c2dbed4df4d
Elton Lee <eltonkwoklee@gmail.com>
2016-05-07 20:10:04 -0400

    Add post req.send stringify object

--- assignmentAJAX/binpost.js
+++ assignmentAJAX/binpost.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', bindPostButton);
 function bindPostButton(){
 document.getElementById("binPost").addEventListener('click', function(event){
   var req = new XMLHttpRequest();
+  var payload = {fname: document.getElementById("fname").value, lname:document.getElementById("lname").value};
   req.open('POST', 'http://httpbin.org/post', true);
   req.setRequestHeader('Content-Type', 'application/json');
   req.addEventListener('load', function(){
@@ -15,7 +16,7 @@ document.getElementById("binPost").addEventListener('click', function(event){
       console.log("Error in network request: "+req.statusText);
     }
   });
-  req.send('{foo:bar, baz:bae}'); //send JSON string-formatted object
+  req.send(JSON.stringify(payload)); //send JSON string-formatted object
   event.preventDefault();
 });
 }
