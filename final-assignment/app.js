f4465c58cc012daed04f15d85123f159c382115c
Elton Lee <eltonkwoklee@gmail.com>
2016-05-26 14:23:30 -0400

    Revert fix

--- final-assignment/app.js
+++ final-assignment/app.js
@@ -142,7 +142,7 @@ app.post('/update', function(req, res) {
         }
         console.log(rows);
         context.results = JSON.stringify(rows);
-        res.render('updateForm',context);
+        res.redirect('updateForm',context);
     });
 });
 
--- final-assignment/public/js/showTable.js
+++ final-assignment/public/js/showTable.js
@@ -85,22 +85,9 @@ function buildTable(data) {
         var updateButton = document.createElement("button");
         updateButton.textContent = "Update";
         updateButton.className = "update";
-        updateButton.addEventListener('click', function(event) {
-            var req = new XMLHttpRequest();
-
-            //The hidden attribute rowId is stored in the lastchild of the row.
-            var rowId = updateButton.parentNode.lastChild.value;
-            console.log("Client-side passed ID: " + rowId);
-
-            //Create payload for POST query.
-            var payload = {
-                id: rowId
-            };
-
-            req.open('POST', 'http://52.37.202.83:3000/update');
-            req.setRequestHeader('Content-Type', 'application/json');
-            req.send(JSON.stringify(payload)); //send JSON string-formatted object
-        });
+        updateButton.onclick= function(){
+          location.href = 'http://52.37.202.83:3000/updateForm/?id='+rowId;
+        };
         newForm.appendChild(updateButton);
 
 
