da56d59f6323ac31427bd369e3203ee1ef2b45f5
Elton Lee <eltonkwoklee@gmail.com>
2016-05-25 21:48:30 -0400

    Add debugging

--- final-assignment/public/js/showTable.js
+++ final-assignment/public/js/showTable.js
@@ -5,6 +5,7 @@ req.setRequestHeader('Content-Type', 'application/json');
 req.addEventListener('load', function() {
     if (req.status >= 200 && req.status < 400) {
         var response = JSON.parse(req.responseText);
+        console.log(response);
         document.body.appendChild(buildTable(response));
 
 
