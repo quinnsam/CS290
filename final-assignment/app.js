f204e9c5e7c3cd658a6e5f3b5296f3ed59d3a7b2
Elton Lee <eltonkwoklee@gmail.com>
2016-05-26 13:22:20 -0400

    Fix handlebars dot notation

--- final-assignment/app.js
+++ final-assignment/app.js
@@ -142,7 +142,7 @@ app.post('/update', function(req, res) {
         }
         console.log(rows);
         context.results = JSON.stringify(rows);
-        res.render('update-form', context);
+        res.render('updateForm', context);
     });
 });
 
